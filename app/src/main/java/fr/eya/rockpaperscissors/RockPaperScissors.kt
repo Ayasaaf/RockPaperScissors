3f2a118d302ed38d38f5dd4b473e377595458dfa
Ayasaaf <eyasaaf81@gmail.com>
2024-12-10 12:31:40 +0100

    first commit

---
+++ app/src/main/java/fr/eya/rockpaperscissors/RockPaperScissors.kt
@@ -0,0 +1,9 @@
+package fr.eya.rockpaperscissors
+
+fun main() {
+    var computerchoice =""
+    var userchoice =""
+    println("Rock,Paper or Scissors ? Enter your choice ")
+    userchoice = readln()
+    val randomNumber = (1..3).random()
+}
